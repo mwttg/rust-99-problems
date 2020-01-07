c555a2da33679ec2a317a1b287d62050593461db
Marco Wittig <marco.wittig@jambit.com>
2020-01-07 09:30:06 +0100

    Solve Problem 02

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,7 @@ fn main() {
 
     println!("Problem 02 - Find the last but one element of a list. Input 'vec![1, 1, 2, 3, 5, 8]'");
     let p01_input = vec![1, 1, 2, 3, 5, 8];
+    let p02_result = p02::penultimate(p01_input);
+    println!("The result (functional) is: {:?}", p02_result);
     println!("-------------------");
 }
---
+++ src/p02/mod.rs
@@ -0,0 +1,50 @@
+/// Find the last but one element of a list
+/// input:
+///   let xs = vec![1, 1, 2, 3, 5, 8]
+///   penultimate(xs)
+/// output:
+///   Some(5)
+
+pub fn penultimate<T>(xs: Vec<T>) -> Option<T> {
+    let length = xs.len();
+    return if length <= 1 {
+        None
+    } else {
+        let position = length - 1 - 1; // counting from zero; penultimate element
+        xs.into_iter().nth(position)
+    }
+}
+
+/// ------------[ Tests ] ---------------------
+#[cfg(test)]
+mod tests {
+    use crate::p02::penultimate;
+
+    #[test]
+    fn penultimate_valid_input_int() {
+        let input = vec![1, 2, 3];
+        let actual = penultimate(input);
+        assert_eq!(actual, Some(2))
+    }
+
+    #[test]
+    fn penultimate_valid_input_string() {
+        let input = vec!["1", "2", "3"];
+        let actual = penultimate(input);
+        assert_eq!(actual, Some("2"))
+    }
+
+    #[test]
+    fn penultimate_valid_empty_input() {
+        let input: Vec<i32> = Vec::new();
+        let actual = penultimate(input);
+        assert_eq!(actual, None)
+    }
+
+    #[test]
+    fn penultimate_valid_input_of_size_one() {
+        let input = vec![5];
+        let actual = penultimate(input);
+        assert_eq!(actual, None)
+    }
+}
